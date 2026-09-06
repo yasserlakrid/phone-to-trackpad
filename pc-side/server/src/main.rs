b76de2a6ffa906dc9d25450edf5c2b6f92a25d17
yasser lakrid <yasserlakrid36@gmail.com>
2026-09-06 14:21:26 +0100

    initial commit

---
+++ pc-side/server/src/main.rs
@@ -0,0 +1,80 @@
+use std::net::TcpListener;
+use std::io::Read;
+use tungstenite::{accept, Message};
+use serde::{Serialize, Deserialize};
+use serde_json;
+use enigo::{Enigo, Mouse, Settings, Coordinate, Button, Direction::{Press, Release, Click} };
+#[derive(Deserialize , Debug)]
+struct MoveInput {
+    move_type : Option<String> , 
+    dx : Option<i32> , 
+    dy : Option<i32> 
+
+}
+fn print_type<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>());
+}
+fn main() {
+
+let listener = TcpListener::bind("0.0.0.0:7878").expect("Failed to bind to address");
+println!("Server listening on port 7878");
+for stream in listener.incoming() {
+    println!("new connection came"); 
+    match stream {
+        Ok(mut stream) => {
+            println!("New connection: {}", stream.peer_addr().unwrap());
+            // Handle the connection in a separate thread or async task
+            std::thread::spawn(move || {
+                let mut enigo = Enigo::new(&Settings::default()).unwrap(); 
+
+              let mut websocket =   match accept(stream) {
+                    Ok(ws) =>{ println!("hanshaked") ; ws },
+                    Err(_err) => {println!("handshake failed"); return ; }
+                 };
+                 loop {
+                    let mut message =  MoveInput {
+                        move_type : None , dx: None , dy : None 
+                    };                     
+                    match websocket.read() {
+                        Ok(Message::Text(text))=>{ message = serde_json::from_str(&text).unwrap(); },
+                        Ok(Message::Binary(data)) => {},
+                        Ok(Message::Close(_)) => { },
+                        Ok(Message::Ping(_)) | Ok(Message::Pong(_)) => { },
+                        Ok(Message::Frame(_)) => {}
+                        Err(err) => { break;
+                            
+                        }
+                    };
+                    
+                    
+                    match message.move_type{
+                        Some(value) => {
+                            if value == "move".to_string() {
+                                enigo.move_mouse(message.dx.unwrap() , message.dy.unwrap() , Coordinate::Abs).unwrap();
+                            }else if value == "left click".to_string() {
+                                enigo.button(Button::Left , Click).unwrap();
+                            }else  {
+                                enigo.button(Button::Right , Click).unwrap();
+                            }
+
+                        },                            
+                       
+                       
+                            
+                        None=>{
+
+                        }
+
+                        
+                    }                     
+                 }
+
+                
+            });
+        }
+        Err(e) => {
+            println!("Connection failed: {}", e);
+        }
+    };
+    }; 
+}
---
+++ phone-side/script.js
@@ -0,0 +1,34 @@
+
+const ws = new WebSocket("ws://127.0.0.1:7878");
+ws.onopen = ()=> console.log("Connected to server")
+ws.onclose   = ()=> console.log("Disconnected from server");
+ws.onerror = (e)=> console.log("Error: ", e);
+
+let trackpad = document.querySelector(".touch");
+let leftBtn = document.querySelector(".left-btn");
+let rightBtn = document.querySelector(".right-btn")
+ws.addEventListener('open', () => {
+     
+trackpad.addEventListener("touchmove", (e) => {
+    e.preventDefault();
+    let dx = e.targetTouches[0].clientX;
+    let dy = e.targetTouches[0].clientY;
+    console.log(JSON.stringify({ type: "move", dx, dy }));
+    ws.send(JSON.stringify({ type: "move", dx, dy }));
+})
+
+trackpad.addEventListener("click", (e) => {
+    e.preventDefault();
+    ws.send(JSON.stringify({ move_type: "left click" }));
+    })
+
+rightBtn.addEventListener("click" , (e)=> {
+    e.preventDefault()
+    ws.send(JSON.stringify({ move_type: "right click" }))
+})
+leftBtn.addEventListener("click" , (e)=> {
+    e.preventDefault()
+    ws.send(JSON.stringify({ move_type: "left click" }))
+})
+
+}) 
